7a37b0e0b756da30070e57b1ee43a9afb9c1479e
Bobby Dias - O Capitão <renatogodias@gmail.com>
2026-09-01 11:11:30 -0300

    build: initialize standalone Android project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Dont-Tread-On-My-Smartphone"
+include(":app")
